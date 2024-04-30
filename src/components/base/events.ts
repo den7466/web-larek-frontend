2d60356fb1691bb0e1984732ce35278e1d9b6b6d
Dmitriy Podorozhnykh <veseliy.koteika@yandex.ru>
2024-04-30 12:45:56 +0300

    Написана документация, описаны базовые типы и интерфейсы. Отправка на первое ревью.

--- src/components/base/events.ts
+++ src/components/base/events.ts
@@ -28,7 +28,7 @@ export class EventEmitter implements IEvents {
     /**
      * Установить обработчик на событие
      */
-    on<T extends object>(eventName: EventName, callback: (event: T) => void) {
+    on<T extends object>(eventName: EventName, callback: (event: T) => void): void {
         if (!this._events.has(eventName)) {
             this._events.set(eventName, new Set<Subscriber>());
         }
@@ -38,7 +38,7 @@ export class EventEmitter implements IEvents {
     /**
      * Снять обработчик с события
      */
-    off(eventName: EventName, callback: Subscriber) {
+    off(eventName: EventName, callback: Subscriber): void {
         if (this._events.has(eventName)) {
             this._events.get(eventName)!.delete(callback);
             if (this._events.get(eventName)?.size === 0) {
@@ -50,7 +50,7 @@ export class EventEmitter implements IEvents {
     /**
      * Инициировать событие с данными
      */
-    emit<T extends object>(eventName: string, data?: T) {
+    emit<T extends object>(eventName: string, data?: T): void {
         this._events.forEach((subscribers, name) => {
             if (name instanceof RegExp && name.test(eventName) || name === eventName) {
                 subscribers.forEach(callback => callback(data));
@@ -61,21 +61,21 @@ export class EventEmitter implements IEvents {
     /**
      * Слушать все события
      */
-    onAll(callback: (event: EmitterEvent) => void) {
+    onAll(callback: (event: EmitterEvent) => void): void {
         this.on("*", callback);
     }
 
     /**
      * Сбросить все обработчики
      */
-    offAll() {
+    offAll(): void {
         this._events = new Map<string, Set<Subscriber>>();
     }
 
     /**
      * Сделать коллбек триггер, генерирующий событие при вызове
      */
-    trigger<T extends object>(eventName: string, context?: Partial<T>) {
+    trigger<T extends object>(eventName: string, context?: Partial<T>): (data: T) => void {
         return (event: object = {}) => {
             this.emit(eventName, {
                 ...(event || {}),
---
+++ src/types/index.ts
@@ -0,0 +1,38 @@
+export interface ICard {
+	id: string;
+	description?: string;
+	image: string;
+	title: string;
+	category: string;
+	price: number | null;
+}
+
+export type TPayment = 'receipt' | 'online';
+
+export interface IOrder {
+  payment: TPayment;
+	email: string;
+	phone: string;
+	address: string;
+	total: number;
+	items: string[];
+}
+
+export interface IOrderModel {
+  order: IOrder;
+    clearOrder: () => void;
+}
+
+export interface IStoreModel {
+	cards: ICard[];
+	getCard: (id: string) => ICard;
+}
+
+export interface IBasketModel {
+	cardsInBasket: ICard[];
+	total: number;
+	existsInBasket: (id: string) => boolean;
+	addToBasket: (card: ICard) => void;
+	removeFromBasket: (id: string) => void;
+	clearBasket: () => void;
+}
